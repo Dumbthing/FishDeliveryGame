493fd445dca9955ed63a88432ea2fa1c18b35652
Jakob <jakob.hust@gmail.com>
2019-09-17 12:51:11 +0200

    test

---
+++ FishDelivery/Assets/Scripts/EventManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+
+    public UnityEvent thisEvent;
+    public UnityEvent affectedEvent;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void thisCollison(GameObject obj)
+    {
+
+    }
+
+    public void thisCollisonEvent(UnityEvent localEvent)
+    {
+
+    }
+
+}
+
+public class EventSystem
+{
+
+
+
+}
